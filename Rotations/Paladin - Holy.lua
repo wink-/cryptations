5267dd7caa1963ac064e479f010c07f2e982a6cf
Dominik Seeger <dominik.seeger@gmx.net>
2017-06-19 15:18:14 +0200

    tank heal logic

--- Rotations/Paladin - Holy.lua
+++ Rotations/Paladin - Holy.lua
@@ -6,6 +6,8 @@ function ct.PaladinHoly()
   local LowestFriend            = ct.FindLowestUnit(ct.friends)
   local HighestFriend           = ct.FindHighestUnit(ct.friends)
 
+  local MainTank, OffTank       = ct.FindTanks()
+
   -- COOLDOWNS
   -- Avenging Wrath (Use when 50% of group is below 70% health)
   if ct.CanCast(31842)
@@ -44,18 +46,81 @@ function ct.PaladinHoly()
   end
 
   -- TANK / SINGLE TARGET HEALING
-  -- Beacon of Light
-  -- Infusion of Light Proc (Either Holy Light or Flash of Light)
-  -- Holy Shock on cooldown (or Light of the Martyr if moving and Holy Shock has CD)
-  -- Bestow Faith on cooldown
-  -- Holy Light (Flash of Light for greater damage)
+  if MainTank ~= nil and MainTank == LowestFriend
+  and ct.PercentHealth(MainTank) <= ct.TankHealthThreshold then
+    -- Beacon of Light (Cast on Main Tank. If BOF is talented, place BOF on lowest unit)
+    if ct.CanCast(53563, MainTank) and not UnitHasAura(MainTank, 53563)
+    and ct.IsInLOS(MainTank) then
+      return ct.AddSpellToQueue(53563, MainTank)
+    end
 
-  -- RAID / AOE HEALING
-  -- Beacon of Light on Tank (If talented, place second beacon on lowest unit)
-  -- Infusion of Light Proc (Either Holy Light or Flash of Light)
-  -- Holy Shock on cooldown (or Light of the Martyr if moving and Holy Shock has CD)
-  -- Judgment (when Judgment of Light is talented)
-  -- Light of Dawn
-  -- Holy Prism or Beacon of Virtue
-  -- Holy Light (Flash of Light for greater damage)
+    -- TODO: remove from here
+    -- Beacon of Faith Talent
+    if LowestFriend ~= nil and ct.CanCast(156910, LowestFriend)
+    and not UnitHasAura(LowestFriend, 156910) and ct.IsInLOS(LowestFriend) then
+      return ct.AddSpellToQueue(156910, LowestFriend)
+    end
+
+    -- Infusion of Light Proc (Either Holy Light or Flash of Light)
+    if ct.UnitHasAura(ct.player, 53576) then
+      if ct.UseHolyLightOnInfusion then
+        if ct.CanCast(82326, MainTank) and ct.IsInLOS(MainTank) then
+          return ct.AddSpellToQueue(82326, MainTank)
+        end
+      elseif ct.UseFlashOfLightOnInfusion then
+        if ct.CanCast(19750, MainTank) and ct.IsInLOS(MainTank) then
+          return ct.AddSpellToQueue(19750, MainTank)
+        end
+      end
+    end
+
+    -- Holy Shock on cooldown (or Light of the Martyr if moving and Holy Shock has CD)
+    if not ct.UnitIsMoving(ct.player) then
+      if ct.CanCast(20473, MainTank) and ct.IsInLOS(MainTank) then
+        return ct.AddSpellToQueue(20473)
+      end
+    else
+      if ct.CanCast(183998, MainTank) and ct.IsInLOS(MainTank) then
+        return ct.AddSpellToQueue(183998)
+      end
+    end
+
+    -- Bestow Faith on cooldown
+    if ct.CanCast(223306, MainTank) and not ct.UnitHasAura(MainTank, 223306)
+    and ct.IsInLOS(MainTank) then
+      return ct.AddSpellToQueue(223306, MainTank)
+    end
+
+    -- Holy Light (Flash of Light for greater damage)
+    if ct.PercentHealth(MainTank) <= ct.FlashOfLightThreshold then
+      if ct.CanCast(19750, MainTank) and ct.IsInLOS(MainTank) then
+        return ct.AddSpellToQueue(19750, MainTank)
+      end
+    else
+      if ct.CanCast(82326, MainTank) and ct.IsInLOS(MainTank) then
+        return ct.AddSpellToQueue(82326, MainTank)
+      end
+    end
+
+    -- RAID / AOE HEALING
+    -- Beacon of Light on Tank (If talented, place second beacon on lowest unit)
+    -- Infusion of Light Proc (Either Holy Light or Flash of Light)
+    -- Holy Shock on cooldown (or Light of the Martyr if moving and Holy Shock has CD)
+    -- Judgment (when Judgment of Light is talented)
+    -- Light of Dawn
+    -- Holy Prism or Beacon of Virtue
+    -- Holy Light (Flash of Light for greater damage)
+  end
+end
+
+function ct.PaldinHolySetUp()
+  -- Infusion of Light Settings
+  ct.UseHolyLightOnInfusion           = true
+  ct.UseFlashOfLightOnInfusion        = false
+
+  -- Health thresholds
+  ct.TankHealthThreshold              = 90
+  ct.OtherHealthThreshold             = 70
+
+  ct.FlashOfLightThreshold            = 50
 end
