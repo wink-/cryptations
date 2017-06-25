01f858620125cd0347fb41993fe2e43d0777a2c9
Dominik Seeger <dominik.seeger@gmx.net>
2017-06-25 19:32:55 +0200

    potential implementation for removing instant spells

--- Core/Core.lua
+++ Core/Core.lua
@@ -6,6 +6,7 @@ ct.Target                   = nil           -- The unit which the player is targ
 ct.Spelltarget              = nil           -- The unit on which a spell shall be casted
 ct.SpellUniqueIdentifier    = 0             -- Every spell will have this value (like a primary key in a database)
 ct.CurrentUniqueIdentifier  = nil           -- This is the primary key of the spell that is currently being casted
+ct.SpellIsInstant           = true
 
 -- GLOBAL SETTINGS
 
@@ -36,12 +37,21 @@ function ct.StartUp()
 
     frame:RegisterEvent("PLAYER_REGEN_ENABLED")
     frame:RegisterEvent("UNIT_SPELLCAST_START")
+    frame:RegisterEvent("UNIT_SPELLCAST_SENT")
+    frame:RegisterEvent("UNIT_SPELLCAST_SUCCEEDED")
 
-    -- This removes the spell currently being casted just before it finished casting
-    local function spellHandler()
+    -- This handles the removing of spells from the spellqueue
+    local function spellDetectionHandler()
+      -- for instant spells
+      if ct.SpellIsInstant and ct.SpellQueue[1] ~= nil
+      and ct.CurrentUniqueIdentifier == ct.SpellQueue[1].key then
+        ct.DeQueueSpell(ct.SpellQueue[1].spell)
+      end
+
+      -- for casted spells
       if not ct.IsCasting(ct.player) and ct.CastedPercent(ct.player) ~= nil
       and ct.SpellQueue[1] ~= nil and ct.CurrentUniqueIdentifier == ct.SpellQueue[1].key then
-        ct.DeQueueSpell(ct.GetSpellID(select(1, UnitCastingInfo(ct.player))))
+        ct.DeQueueSpell(ct.SpellQueue[1].spell)
         -- Spell History
         -- maximum lenght of spell history is 10 entries
         if getn(ct.SpellHistory) > 10 then
@@ -52,20 +62,26 @@ function ct.StartUp()
         local Entry = {spell = arg5, time = GetTime()}
         table.insert(ct.SpellHistory, Entry)
       end
+
+      ct.SpellIsInstant = true
     end
 
     local function eventHandler(self, event, arg1, arg2, arg3, arg4, arg5, arg6)
       if event == "UNIT_SPELLCAST_START" and arg1 == "player" and getn(ct.SpellQueue) ~= 0 then
         ct.CurrentUniqueIdentifier = ct.SpellQueue[1].key
+        ct.SpellIsInstant = false
       end
       if event == "PLAYER_REGEN_ENABLED" then
-        -- player left any combat action (or the target died) so the queue will be cleaned up
+        -- player left any combat action so the queue will be cleaned up
         ct.CleanUpQueue()
+      end
+      if event == "UNIT_SPELLCAST_SENT" and arg1 == "player" and getn(ct.SpellQueue) ~= 0 then
+        ct.CurrentUniqueIdentifier = ct.SpellQueue[1].key
       end
     end
 
     frame:SetScript("OnEvent", eventHandler)
-    spellframe:SetScript("OnUpdate", spellHandler)
+    spellframe:SetScript("OnUpdate", spellDetectionHandler)
 
     -- define player object (needed for ewt)
     ct.player = GetObjectWithGUID(UnitGUID("player"))
