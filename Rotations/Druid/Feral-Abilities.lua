4db26d6e1a5635ae33c6efe0fad5b685458a7fee
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-09 17:11:16 +0200

    started working on feral druid

--- Rotations/Druid/Feral-Abilities.lua
+++ Rotations/Druid/Feral-Abilities.lua
@@ -4,3 +4,63 @@ local SpecID  = GetSpecialization()
 if ClassID ~= 11 then return end
 if SpecID ~= 2 then return end
 if FireHack == nil then return end
+
+local Unit        = LibStub("Unit")
+local Spell       = LibStub("Spell")
+local Rotation    = LibStub("Rotation")
+local Player      = LibStub("Player")
+local Buff        = LibStub("Buff")
+local Debuff      = LibStub("Debuff")
+local BossManager = LibStub("BossManager")
+
+function DFRakeV1()
+  if PlayerTarget ~= nil
+  and Spell.CanCast(1822, PlayerTarget, 3, 35) then
+    if Buff.Has(PlayerUnit, 5215)
+    or Buff.Has(PlayerUnit, 58984) then
+      return Spell.Cast(1822, PlayerTarget)
+    end
+  end
+end
+
+function DFTigersFury()
+  if Spell.CanCast(5217) then
+    if not Buff.Has(PlayerUnit, 16870)
+    and MaxEnergy - Energy >= 60
+    or MaxEnergy - Energy >= 80 then
+      return Spell.Cast(5217)
+  end
+end
+
+function DFIKotJ()
+  if Spell.CanCast(102543)
+  and Buff.Has(PlayerUnit, 5217) then
+    return Spell.Cast(102543)
+  end
+end
+
+function DFBerserk()
+  if Spell.CanCast(106951)
+  and Buff.Has(PlayerUnit, 5217) then
+    return Spell.Cast(106951)
+  end
+end
+
+function DFFerociousBiteMaxEnergy()
+  local EnergyNeeded = 25
+  if Buff.Has(PlayerUnit, 106951) then
+    EnergyNeeded = EnergyNeeded / 2
+  elseif Buff.Has(PlayerUnit, 102543) then
+    EnergyNeeded = EnergyNeeded * 0.4
+  end
+
+  return EnergyNeeded * 2
+end
+
+function DFElunesGuidance()
+  if Spell.CanCast(202060)
+  and ComboPoints <= 1
+  and Energy >= DFFerociousBiteMaxEnergy() then
+    return Spell.Cast(202060)
+  end
+end
--- Rotations/Druid/Feral.lua
+++ Rotations/Druid/Feral.lua
@@ -16,7 +16,82 @@ end
 
 local Settings = json.decode(content)
 
+Interrupt   = Settings.Interrupt
+
+local Unit        = LibStub("Unit")
+local Spell       = LibStub("Spell")
+local Rotation    = LibStub("Rotation")
+local Player      = LibStub("Player")
+local Buff        = LibStub("Buff")
+local Debuff      = LibStub("Debuff")
+local BossManager = LibStub("BossManager")
+
+function Finishers()
+  -- Rip
+  -- Ferocious Bite
+  -- Rip 2
+  -- Savage Roar
+  -- Maim
+  -- Ferocious Bite 2
+  -- Rip 3
+end
+
+function AoE()
+  -- Thrash
+  -- Brutal Slash
+  -- Thrash 2
+  -- Swipe
+end
+
+function Generators()
+  -- Shadowmeld
+  -- Rake
+  -- Rake 2
+  -- Rage 3
+  -- Rake 4
+  -- Brutal Slash
+  -- Moonfire
+  -- Thrash
+  -- Swipe
+  -- Thrash 3
+  -- Thrash 4
+  -- Shred
+end
+
 function Pulse()
+  -- combat rotation
+  if UnitAffectingCombat(PlayerUnit)
+  or (AllowOutOfCombatRoutine and UnitGUID("target") ~= nil
+  and Unit.IsHostile("target")) and UnitHealth("target") ~= 0 then
+
+    -- pulse target engine and remember target
+    Rotation.Target("hostile")
+    PlayerTarget = GetObjectWithGUID(UnitGUID("target"))
+    TTD = Unit.ComputeTTD(PlayerTarget)
+
+    Energy          = UnitPower("player", 3)
+    MaxEnergy       = UnitPowerMax("player", 3)
+    ComboPoints     = UnitPower("player", 4)
+    MaxComboPoints  = UnitPowerMax("player", 4)
+
+    -- call interrupt engine
+    if Interrupt then
+      Rotation.Interrupt()
+    end
+
+    DFRakeV1()
+    DFTigersFury()
+    DFIKotJ()
+    DFBerserk()
+    -- Cooldowns
+    DFElunesGuidance()
+    -- Ferocious Bite
+    -- Regrowth
+    -- Regrowth 2
+    Finishers()
+    -- Ashamane's Frenzy
+    AoE()
+    Generators()
 end
 
 function Interrupt()
