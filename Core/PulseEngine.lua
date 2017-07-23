c0285661c4c6fa0f8fbf1de5f47487ed1395e492
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-23 21:09:08 +0200

    removed pulse giver from xml file

--- Core/PulseEngine.lua
+++ Core/PulseEngine.lua
@@ -1,24 +1,16 @@
-UpdateInterval = 0.1
-
 local Rotation = LibStub("Rotation")
 
 function PulseEngine(self, elapsed)
-  self.TimeSinceLastUpdate = self.TimeSinceLastUpdate + elapsed
-
-  while (self.TimeSinceLastUpdate > UpdateInterval and UpdateInterval ~= 0) do
-
-    if FireHack == nil then
-      UpdateInterval = 0
-      return message("No Unlocker Loaded. Attatch Unlocker and Reload")
-    end
-
-    -- TODO: pulse engine delays for:
-    -- when player left combat
-    -- when player is looting
-
-    -- Pulse the Queue
-    Rotation.PulseQueue()
-
-    self.TimeSinceLastUpdate = self.TimeSinceLastUpdate - UpdateInterval
+  if FireHack == nil then
+    return message("No Unlocker Loaded. Attatch Unlocker and Reload")
   end
+
+  -- TODO: pulse engine delays for:
+  -- when player left combat
+  -- when player is looting
+
+  -- Pulse the Queue
+  Rotation.PulseQueue()
 end
+
+AddTimerCallback(0.1, PulseEngine)
