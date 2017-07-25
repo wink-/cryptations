5e53d7c0bf6e41997a0d5069eb156096e3d3bf9c
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-25 13:29:47 +0200

    removed unused locals

--- Rotations/Paladin/Holy.lua
+++ Rotations/Paladin/Holy.lua
@@ -58,8 +58,6 @@ local Unit        = LibStub("Unit")
 local Spell       = LibStub("Spell")
 local Rotation    = LibStub("Rotation")
 local Player      = LibStub("Player")
-local Buff        = LibStub("Buff")
-local Debuff      = LibStub("Debuff")
 local BossManager = LibStub("BossManager")
 local Utils       = LibStub("Utils")
 
