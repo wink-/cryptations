dc01e2f61b3c4da6071447a761a630f464d59c21
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-25 16:24:42 +0200

    introduced spellbooks

---
+++ Rotations/Druid/SpellBook.lua
@@ -0,0 +1,8 @@
+local ClassID = select(3, UnitClass("player"))
+
+if ClassID ~= 11 then return end
+if FireHack == nil then return end
+
+SB = {
+  
+}
---
+++ Rotations/Paladin/SpellBook.lua
@@ -0,0 +1,9 @@
+local ClassID = select(3, UnitClass("player"))
+local SpecID  = GetSpecialization()
+
+if ClassID ~= 2 then return end
+if FireHack == nil then return end
+
+SB = {
+  
+}
