2bb585600ec8bf6f9f817a0f67a5d3b5de240896
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-30 21:41:34 +0200

    changed to bitflags

--- Functions/Utils.lua
+++ Functions/Utils.lua
@@ -7,6 +7,13 @@ end
 
 -- returns the z coordinate that is just above the ground at given position
 function Utils.FindGroundAt(x, y, z)
-  local hx, hy, hz = TraceLine(x, y, z, x, y, z - 100, {HitFlags.Terrain, HitFlags.WmoCollision, HitFlags.WmoRender})
+  local hx, hy, hz = TraceLine(x, y, z, x, y, z - 100, {0x100, 0x10, 0x20})
   return hx, hy, hz
+end
+
+-- returns the x, y, z coordinates of the best spot to cast an aoe on
+-- maxDistance: maximum distance from player
+-- radius: radius of effect of the spell
+function Utils.FindBestAoESpot(maxDistance, radius)
+  -- TODO
 end
