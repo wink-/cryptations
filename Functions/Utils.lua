ef6c2ea1b65867aafe91ea6d7b778b393d4d3458
Dominik Seeger <dominik.seeger@gmx.net>
2017-07-30 21:35:18 +0200

    introduced FindGroundAt()

--- Functions/Utils.lua
+++ Functions/Utils.lua
@@ -5,7 +5,8 @@ function Utils.GetDistanceBetweenPositions(x1, y1, z1, x2, y2, z2)
   return math.sqrt(math.pow(X2 - X1, 2) + math.pow(Y2 - Y1, 2) + math.pow(Z2 - Z1, 2));
 end
 
--- returns the z coordinate of the ground at position x, y
-function Utils.FindGroundAtPosition(x, y)
-  -- TODO
+-- returns the z coordinate that is just above the ground at given position
+function Utils.FindGroundAt(x, y, z)
+  local hx, hy, hz = TraceLine(x, y, z, x, y, z - 100, {HitFlags.Terrain, HitFlags.WmoCollision, HitFlags.WmoRender})
+  return hx, hy, hz
 end
