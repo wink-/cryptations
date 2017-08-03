fd24b0c24e0516ebb7b3dffd81a4110dd68d8d88
Dominik Seeger <dominik.seeger@gmx.net>
2017-08-03 19:53:39 +0200

    improved keylistener

--- Functions/Events.lua
+++ Functions/Events.lua
@@ -137,6 +137,8 @@ Keys = {
 -- This function "listens" to any key input
 -- If there is a Callback function assigned for a certain "HotKey" it will be executed
 -- Every class must have a table called "KeyCallbacks" where this function can access to
+local KeyDelay = 0.5
+local NextExecute = GetTime()
 function Events.KeyListener()
   for k, v in pairs(KeyCallbacks) do
     local KeysToCheck = {strsplit(",", k)}
@@ -147,8 +149,10 @@ function Events.KeyListener()
       end
     end
 
-    if AllPressed then
-      print("function is being executed")
+    if AllPressed
+    and GetTime() >= NextExecute then
+      NextExecute = GetTime() + KeyDelay
+      v()
     end
   end
 end
